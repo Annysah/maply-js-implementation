dedb63f4cbeceae49b297c31f168076b8b08d80b
Anisat Akinbani <anisatakinbani13@gmail.com>
2022-04-21 23:05:21 +0100

    Updaated this repo

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent
+
 if (navigator.geolocation)
   //To get the coordinates for a user location
   navigator.geolocation.getCurrentPosition(
@@ -23,30 +25,17 @@ if (navigator.geolocation)
 
       //To get the map from the leaflet library
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng
-
-        L.marker({ lat, lng })
-          .addTo(map)
-          .bindPopup(
-            //Maker popup property
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup'
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+      map.on('click', function (mapEventLocal) {
+        mapEvent = mapEventLocal
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
 
@@ -54,3 +43,22 @@ if (navigator.geolocation)
       alert('Could not get your current location');
     }
   );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const { lat, lng } = mapEvent.latlng
+  L.marker({ lat, lng })
+    .addTo(map)
+    .bindPopup(
+      //Maker popup property
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
